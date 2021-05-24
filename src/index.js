c2eb54a232aa6caf798312ab42ef2ddc91ae3d2d
Swayambhu872 <swayambhunathan@gmail.com>
2021-05-24 23:53:22 +0530

    Implemented Login page functionality

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Controller from './screens/Controller';
+import Controller from '../src/common/Controller';
 
 
 import reportWebVitals from './reportWebVitals';
---
+++ src/screens/home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component {
+    render() {
+        return(
+            <div>
+                <span>
+                    Home Page
+                </span>
+            </div>
+        )
+    }
+}
+export default Home;
